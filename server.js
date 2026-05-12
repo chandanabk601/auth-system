e7071d921ba56a011f4cd9db0badf0739a364ec1
chandanabk601 <chandanabk601@gmail.com>
2026-05-12 18:09:51 +0530

    Added project code

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server Running");
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+app.use("/api/auth", require("./routes/authRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
